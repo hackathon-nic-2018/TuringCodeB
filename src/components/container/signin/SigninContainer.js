3169da2ec16fd17fb33e677aa2dc9a35e12abeb2
Luis Solorzano <luisnic20@gmail.com>
2018-11-30 08:22:19 -0600

    Create component Signin

---
+++ src/components/container/signin/SigninContainer.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+
+export default class SigninContainer extends Component {
+  render () {
+    const image = '/assets/media/img/profile/zoneg.png'
+    const bg_profile = {
+      background: 'url(' + image + ')'
+    }
+
+    return (
+      <div className={'turing--content--profile'}>
+        <div className={'container-fluid turing--profile-bg'} style={bg_profile}/>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className={'container'}>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mx-auto">
+                <a className="navbar-brand font-weight-bold" href="#">Business Name</a>
+
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className={'container'}>
+          <table className="table">
+            <thead className="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+            </tbody>
+          </table>
+
+          <table className="table">
+            <thead className="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/ui/business/ChipsBusiness.js
+++ src/components/ui/business/ChipsBusiness.js
@@ -6,23 +6,23 @@ export default class ChipsBusiness extends Component {
     return (
       <div className={'container'}>
         <div className='row justify-content-center'>
-          <a href={'/business/tourims'} type={'tourims'} className={'turing-tourism shadow'}>
+          <a href={'/business/tourims'} type={'tourims'} className={'turing-tourism shadow fadeInLeft animated'}>
             <i className={'fal fa-umbrella-beach fa-lg'}/> T&uacute;rismo
           </a>
 
-          <a href={'/business/bar_and_restaurant'} type={'bar_and_restaurant'} className={'turing-gastronomy shadow'}>
+          <a href={'/business/bar_and_restaurant'} type={'bar_and_restaurant'} className={'turing-gastronomy shadow fadeInLeft animated'}>
             <i className={'fal fa-utensils fa-lg'}/> Bar y Restaurantes
           </a>
 
-          <a href={'/business/hotel'} type={'hotel'} className={'turing-hotels shadow'}>
+          <a href={'/business/hotel'} type={'hotel'} className={'turing-hotels shadow bounce animated'}>
             <i className={'fal fa-hotel fa-lg'}/> Hoteles
           </a>
 
-          <a href={'/business/establishments'} type={'establishments'} className={'turing-store shadow'}>
+          <a href={'/business/establishments'} type={'establishments'} className={'turing-store shadow fadeInRight animated'}>
             <i className={'fal fa-house-flood fa-lg'}/> Establecimientos
           </a>
 
-          <a href={'/business/small_business'} type={'small_business'} className={'turing-business shadow'}>
+          <a href={'/business/small_business'} type={'small_business'} className={'turing-business shadow fadeInRight animated'}>
             <i className={'fal fa-store fa-lg'}/> Peque&ntilde;os negocios
           </a>
         </div>
